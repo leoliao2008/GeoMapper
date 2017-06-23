6066ee4082d89a484840fcdfd1b405eb1ff2eea2
leoliao2008@163.com <leoliao2008@163.com>
2017-06-24 02:23:46 +0800

    modification from home 05

--- app/src/main/java/com/skycaster/geomapper/customized/SatelliteMapView.java
+++ app/src/main/java/com/skycaster/geomapper/customized/SatelliteMapView.java
@@ -16,6 +16,7 @@ import android.view.SurfaceView;
 import com.skycaster.geomapper.R;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by 廖华凯 on 2017/6/23.
@@ -24,9 +25,11 @@ import java.util.ArrayList;
 public class SatelliteMapView extends SurfaceView {
 
     private SurfaceHolder mSurfaceHolder;
-    private float radius;
+    private float innerRadius;
+    private float outerRadius;
     private int canvasWidth;
     private int canvasHeight;
+    private Paint mPaintDarkBlue;
     private Paint mPaintBlue;
     private Paint mPaintWhiteSolid;
     private Paint mPaintWhiteDot;
@@ -35,6 +38,8 @@ public class SatelliteMapView extends SurfaceView {
     private TextPaint mTextPaintFontBig;
     private float centerX;
     private float centerY;
+    private AtomicBoolean isFirstTimeDrawn=new AtomicBoolean(true);
+    private double mFontHeight;
 
     public SatelliteMapView(Context context) {
         this(context,null);
@@ -48,11 +53,14 @@ public class SatelliteMapView extends SurfaceView {
         super(context, attrs, defStyleAttr);
         mSurfaceHolder = getHolder();
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SatelliteMapView);
-        radius=typedArray.getDimension(R.styleable.SatelliteMapView_radius,400);
+        innerRadius =typedArray.getDimension(R.styleable.SatelliteMapView_radius, 400);
         typedArray.recycle();
 
         mPaintBlue =new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBlue.setColor(Color.parseColor("#00003E"));
+
+        mPaintDarkBlue=new Paint(mPaintBlue);
+        mPaintDarkBlue.setColor(Color.parseColor("#01152E"));
 
 
         mPaintWhiteSolid =new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -71,6 +79,9 @@ public class SatelliteMapView extends SurfaceView {
         mTextPaintFontBig=new TextPaint(mTextPaintFontSmall);
         mTextPaintFontBig.setTextSize(getResources().getDimension(R.dimen.text_size_for_satellite_view_big));
         mTextPaintFontBig.setStrokeWidth(4);
+        Paint.FontMetrics fontMetrics = mTextPaintFontBig.getFontMetrics();
+        mFontHeight = Math.ceil(fontMetrics.descent - fontMetrics.ascent-fontMetrics.leading*2);
+        outerRadius= (float) (innerRadius + mFontHeight);
 
         mPaintSatellite=new Paint(Paint.ANTI_ALIAS_FLAG);
 
@@ -92,6 +103,7 @@ public class SatelliteMapView extends SurfaceView {
 
             }
         });
+
         setLayerType(LAYER_TYPE_SOFTWARE,null);
     }
 
@@ -104,25 +116,37 @@ public class SatelliteMapView extends SurfaceView {
         centerY=canvasHeight/2;
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if(isFirstTimeDrawn.compareAndSet(true,false)){
+            drawCompass(canvas);
+        }
+    }
+
     /**
      * 绘制罗盘
      * @param canvas
      */
     private void drawCompass(Canvas canvas) {
-        //画背景实心圆
-        canvas.drawCircle(centerX,centerY,radius, mPaintBlue);
-        //画最外层圈圈
-        canvas.drawCircle(centerX,centerY,radius, mPaintWhiteSolid);
-        //画内部两个虚线圈圈
-        for(int i=1;i<3;i++){
-            canvas.drawCircle(centerX,centerY,radius/3*i,mPaintWhiteDot);
+
+        //画外部实心圆
+        canvas.drawCircle(centerX, centerY, outerRadius, mPaintDarkBlue);
+        //画内部实心圆
+        canvas.drawCircle(centerX, centerY, innerRadius, mPaintBlue);
+        //画最外层两个实线圈圈
+        canvas.drawCircle(centerX, centerY, outerRadius, mPaintWhiteSolid);
+        canvas.drawCircle(centerX, centerY, innerRadius, mPaintWhiteSolid);
+        //画内部4个虚线圈圈
+        for(int i=1;i<5;i++){
+            canvas.drawCircle(centerX, centerY, innerRadius / 5 * i, mPaintWhiteDot);
         }
-        //分别画12条标识角度的虚线，以圆心为终点
-        for(int i=1;i<13;i++){
-            int degree = 30 * i;
+        //分别画24条标识角度的虚线，以圆心为终点
+        for(int i=1;i<25;i++){
+            int degree = 15 * i;
             double lineAngle=degree*Math.PI/180;
-            float startX= (float) (centerX+Math.cos(lineAngle)*(radius));
-            float startY= (float) (centerY-Math.sin(lineAngle)*(radius));
+            float startX= (float) (centerX+Math.cos(lineAngle)*(innerRadius));
+            float startY= (float) (centerY-Math.sin(lineAngle)*(innerRadius));
             float stopX=centerX;
             float stopY=centerY;
             canvas.drawLine(startX,startY,stopX,stopY,mPaintWhiteDot);
@@ -134,15 +158,24 @@ public class SatelliteMapView extends SurfaceView {
                     info="N";
                     textPaint=mTextPaintFontBig;
                     break;
-                case 360:
-                    info="0°";
-                    break;
                 default:
-                    info=degree +"°";
+                    int temp=90-degree;
+                    if(temp<0){
+                        temp+=360;
+                    }
+                    info=temp +"°";
                     break;
             }
-            startX= (float) (centerX+Math.cos(lineAngle)*(radius+10));
-            startY= (float) (centerY-Math.sin(lineAngle)*(radius+10));
+            Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+            double deviation= Math.ceil(fontMetrics.descent - fontMetrics.ascent-fontMetrics.leading*2)/2;
+            if(degree!=90){
+                startX= (float) (centerX+Math.cos(lineAngle)*(innerRadius + mFontHeight/2-deviation));
+                startY= (float) (centerY-Math.sin(lineAngle)*(innerRadius + mFontHeight/2-deviation));
+            }else {
+                startX= (float) (centerX+Math.cos(lineAngle)*(innerRadius + 15));
+                startY= (float) (centerY-Math.sin(lineAngle)*(innerRadius + 15));
+            }
+
             //旋转文字使其方向与园外径相切
             canvas.rotate(90-degree,startX,startY);
             float textWidth = textPaint.measureText(info);
@@ -161,7 +194,7 @@ public class SatelliteMapView extends SurfaceView {
         drawCompass(canvas);
         for(GpsSatellite satellite:list){
             float elevation = satellite.getElevation();
-            float r = radius * (elevation / 90);
+            float r = innerRadius * (elevation / 90);
             double degree = (90 - satellite.getAzimuth() + 360) * Math.PI / 180;
             float x= (float) (centerX+Math.cos(degree)*r);
             float y= (float) (centerY+Math.sin(degree)*r);
