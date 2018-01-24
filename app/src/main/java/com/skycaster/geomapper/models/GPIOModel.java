6cb67a22fe146012346dc0bf53512394952c4312
leoliao2008@163.com <leoliao2008@163.com>
2018-01-24 15:21:42 +0800

    company change 114

--- app/src/main/java/com/skycaster/geomapper/models/GPIOModel.java
+++ app/src/main/java/com/skycaster/geomapper/models/GPIOModel.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class GPIOModel {
 
     private static final String GPIO_CONTROL_POW = "/sys/class/gpio/gpio205/value"; //控制CDRadio芯片的电源
-    private static final String GPIO_CONTROL_FRQ="/sys/class/gpio/gpio216/value";//控制CDRadio芯片的串口连接方向（通向手机主板电路或北斗模块电路）
+    private static final String GPIO_CONTROL_DIRECTION ="/sys/class/gpio/gpio216/value";//控制CDRadio芯片的串口连接方向（通向手机主板电路或北斗模块电路）
 
     /**
      * CDRadio芯片上电
@@ -35,14 +35,14 @@ public class GPIOModel {
      * 连接CDRadio芯片和手持机的串口
      */
     public void connectCdRadioToCPU() throws IOException {
-        writeFile(GPIO_CONTROL_FRQ,"1".getBytes());
+        writeFile(GPIO_CONTROL_DIRECTION,"1".getBytes());
     }
 
     /**
      * 连接CDRadio芯片和北斗芯片的串口
      */
     public void connectCDRadioToBeidou() throws IOException {
-        writeFile(GPIO_CONTROL_FRQ,"0".getBytes());
+        writeFile(GPIO_CONTROL_DIRECTION,"0".getBytes());
     }
     private void writeFile(String path, byte[] buffer) throws IOException {
         File file = new File(path);
